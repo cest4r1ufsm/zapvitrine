269d4ea559a34aaa3315c5347adf3ba6d6f83bec
cest4r1ufsm <cest4r1ufsm@users.noreply.github.com>
2026-07-01 23:14:31 -0300

    refactor(client): update vite config with host and allowedHosts for VPS

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5007,
+    host: true,
+    allowedHosts: true,
+    cors: true,
+  },
+  preview: {
+    port: 5009,
+    host: true,
+    allowedHosts: true,
+    cors: true,
+  },
 })
